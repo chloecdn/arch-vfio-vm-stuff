124cbef2d0a764b03328e0d7e5cae69fb91f0586
Chloe <chloe@chloedev.de>
2023-03-17 03:14:27 +0100

    Create isolate.sh

---
+++ isolate.sh
@@ -0,0 +1,11 @@
+wget -O /usr/local/bin/vfio-pci-override.sh https://raw.githubusercontent.com/chloecdn/arch-vfio-vm-stuff/main/vfio-pci-override.sh
+
+chmod +x /usr/local/bin/vfio-pci-override.sh
+
+read -p 'Are you using Btrfs? [y/n] ' isBtrfs
+
+if ( $isBtrfs == "y" ); then
+  wget -O /etc/mkinitcpio.conf https://raw.githubusercontent.com/chloecdn/arch-vfio-vm-stuff/main/etc/mkinitcpio.conf.btrfs
+elif ( $isBtrfs == "n" ); then
+  wget -O /etc/mkinitcpio.conf https://raw.githubusercontent.com/chloecdn/arch-vfio-vm-stuff/main/etc/mkinitcpio.conf
+fi
