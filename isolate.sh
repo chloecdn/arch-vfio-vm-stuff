f05a2a6f1ed894b1fafe1f1037bbb7be6f2c46a4
Chloe <chloe@chloedev.de>
2023-03-17 03:29:59 +0100

    Update isolate.sh

--- isolate.sh
+++ isolate.sh
@@ -7,6 +7,6 @@ while true; do
     case $yn in
         [Yy]* ) wget -O /etc/mkinitcpio.conf https://raw.githubusercontent.com/chloecdn/arch-vfio-vm-stuff/main/etc/mkinitcpio.conf.btrfs;;
         [Nn]* ) wget -O /etc/mkinitcpio.conf https://raw.githubusercontent.com/chloecdn/arch-vfio-vm-stuff/main/etc/mkinitcpio.conf;;
-        * ) echo "Are you using Btrfs? [y/n]";;
+        * );;
     esac
 done
