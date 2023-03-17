d886ee69b8b25c10e407e09ceb3aae5aa3ddaeeb
Chloe <chloe@chloedev.de>
2023-03-17 03:23:01 +0100

    Update isolate.sh

--- isolate.sh
+++ isolate.sh
@@ -2,10 +2,11 @@ wget -O /usr/local/bin/vfio-pci-override.sh https://raw.githubusercontent.com/ch
 
 chmod +x /usr/local/bin/vfio-pci-override.sh
 
-read -p 'Are you using Btrfs? [y/n] ' isBtrfs
-
-if [ "$isBtrfs" = "y" ]; then
-  wget -O /etc/mkinitcpio.conf https://raw.githubusercontent.com/chloecdn/arch-vfio-vm-stuff/main/etc/mkinitcpio.conf.btrfs
-elif [ "$isBtrfs" = "n" ]; then
-  wget -O /etc/mkinitcpio.conf https://raw.githubusercontent.com/chloecdn/arch-vfio-vm-stuff/main/etc/mkinitcpio.conf
-fi
+while true; do
+    read -p "Are you using Btrfs? [y/n]" yn
+    case $yn in
+        [Yy]* ) wget -O /etc/mkinitcpio.conf https://raw.githubusercontent.com/chloecdn/arch-vfio-vm-stuff/main/etc/mkinitcpio.conf.btrfs;;
+        [Nn]* ) wget -O /etc/mkinitcpio.conf https://raw.githubusercontent.com/chloecdn/arch-vfio-vm-stuff/main/etc/mkinitcpio.conf;;
+        * ) echo "Are you using Btrfs? [y/n]";;
+    esac
+done
